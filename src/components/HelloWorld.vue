467a24948ffc4850fe796a9d9f3b99c0ab18426e
Hoang Thi Ha <ha.ht@neo-lab.vn>
2020-03-19 12:41:12 +0700

    Add new project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <div id="app1">
+      <p>{{ msg }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
